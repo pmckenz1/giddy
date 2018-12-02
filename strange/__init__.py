c4973f572d5ef058a12dad843b6320d2d027ef56
eaton-lab <de2356@columbia.edu>
2018-12-01 23:30:23 -0500

    dev version

--- strange/__init__.py
+++ strange/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-__version__ = "0.1"
+__version__ = "0.2-dev"
 __author__ = "Patrick McKenzie"
-from .Coalseq import Coalseq, get_dists
+from .Coalseq import Coalseq
 from .Window import Window
