37beef68f441548a5ad710b0edf113c5a5431c1f
eaton-lab <de2356@columbia.edu>
2018-12-02 17:01:23 -0500

    up version and import SlidingWindow

--- strange/__init__.py
+++ strange/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-__version__ = "0.2-dev"
+__version__ = "0.2.1-dev"
 __author__ = "Patrick McKenzie"
 from .Coalseq import Coalseq
-from .Window import Window
+from .Window import Window, SlidingWindow
