3df468c707e6e77d5268922345695e4732de406a
eaton-lab <de2356@columbia.edu>
2019-01-27 13:21:53 -0500

    added compare to init

--- strange/__init__.py
+++ strange/__init__.py
@@ -5,5 +5,5 @@ __author__ = "Patrick McKenzie"
 
 from .Coalseq import Coalseq
 from .Window import SlidingWindow
-# from .MCMC import *
+from .MCMC import MCMC, Compare
 # from .Window import MBmcmc
