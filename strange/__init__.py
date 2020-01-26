8faf506fd6aa97445574adeb6848b2066c6beacc
eaton-lab <de2356@columbia.edu>
2020-01-26 11:07:13 -0500

    update to sptree

--- strange/__init__.py
+++ strange/__init__.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 
 __version__ = "0.2.1-dev"
-__author__ = "Patrick McKenzie"
-from .Coalseq import Coalseq
-from .Window import Window, SlidingWindow
+__author__ = "Deren Eaton"
+
+
+from .clade_table import *
+
+# mostly replaced by ipcoal now
+# from .Coalseq import Coalseq
+# from .Window import Window, SlidingWindow
